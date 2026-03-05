2d87688836737680a732a50e9c9ee2c4b467dd77
Amyholst1 <a_amy@live.se>
2026-03-05 14:10:50 +0100

    setup firebase start delete endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
-import express from 'express'
+import express from "express";
+import { db } from "./database.js";
 
-console.log("server.js körs ✅");
+console.log("firestore connected")
 
+<<<<<<< Updated upstream
 const app = express()
 const PORT = 3002
+=======
+>>>>>>> Stashed changes
+
+const app = express();
+const PORT = 3000;
 
 app.get('/get', (req, res) => {
     res.send("Hej grupp 2")
 } )
+
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const todoID = req.params.id
+
+        await db.collection("")
+    } catch {
+
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on: http://${PORT}`)
