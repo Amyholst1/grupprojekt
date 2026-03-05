a35773d2992f9af2958b2413aed169fea5865bbf
Prasoon <prasoons97@gmail.com>
2026-03-05 22:43:34 +0100

    feat: Implement addTodo

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,76 @@
 import express from "express";
-import { db } from "./database.js";
+import { db, FieldValue } from "./database.js";
 
 const app = express();
 const PORT = 3002;
 
-
 app.use(express.json());
 
-
 app.get("/getTodos", async (req, res) => {
-    try {
-        const todoCollection = db.collection("Todos")
-        const snapshot = await todoCollection.get()
+  try {
+    const todoCollection = db.collection("Todos");
+    const snapshot = await todoCollection.get();
 
-        if(snapshot.empty) {
-            return res.status(400).json([])
-        }
-
-        let todos = []
-
-        snapshot.forEach(todo => {
-            todos.push({
-                id: todo.id,
-                ...todo.data(),
-            })
-        })
-
-        res.status(200).json(todos)
-
-    } catch (error) {
-        console.log("Fel i hämtning av Todos")
-        res.status(500).json({ error: "Fel i hämtning av Todos" })
+    if (snapshot.empty) {
+      return res.status(400).json([]);
     }
-})
 
+    let todos = [];
+
+    snapshot.forEach((todo) => {
+      todos.push({
+        id: todo.id,
+        ...todo.data(),
+      });
+    });
+
+    res.status(200).json(todos);
+  } catch (error) {
+    console.log("Fel i hämtning av Todos");
+    res.status(500).json({ error: "Fel i hämtning av Todos" });
+  }
+});
 
 app.delete("/deleteTodo/:id", async (req, res) => {
-    try {
-        const todoID = req.params.id
+  try {
+    const todoID = req.params.id;
 
-        await db.collection("Todos").doc(todoID).delete()
+    await db.collection("Todos").doc(todoID).delete();
 
-        res.status(200).send("Borttagen")
-    } catch (error) {
-        console.log("Fel vid borttagning av Todo i servern", error)
-        res.status(500).send("Något gick fel i servern med Delete")
+    res.status(200).send("Borttagen");
+  } catch (error) {
+    console.log("Fel vid borttagning av Todo i servern", error);
+    res.status(500).send("Något gick fel i servern med Delete");
+  }
+});
+
+app.post("/addTodo", async (req, res) => {
+  try {
+    const { title, completed } = req.body;
+
+    // Validation
+    if (!title) {
+      return res.status(400).send("Title missing");
     }
-})
+
+    const newTodo = {
+      title: title,
+      completed: completed ?? false,
+      createdAt: FieldValue.serverTimestamp(),
+    };
+
+    const docRef = await db.collection("Todos").add(newTodo);
+
+    res.status(200).json({
+      id: docRef.id,
+      title: title,
+      completed: completed ?? false,
+    });
+  } catch (error) {
+    console.log("Del vid skapandet av Todo.", error);
+    res.status(500).send(error.message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
