6eae3bbeed537a92cf198c4eb2aa357b5bcb7288
JK Nordstrrand <nordstrand@MacBook-Pro-som-tillhor-JK.local>
2026-03-05 09:49:24 +0100

    add footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      
+      <p>Copyright © 2026</p>
+
+      <div className="theme-toggle">
+        <button>☀️</button>
+        <button>🌙</button>
+      </div>
+
+    </footer>
+  )
+}
+
+export default Footer
