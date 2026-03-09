54daa0e5743d9df7630126fccae92089602430d1
JK Nordstrand <joohannees@hotmail.se>
2026-03-09 10:55:50 +0100

    Task 7: add GET endpoint for todos

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,29 @@
 import express from "express";
 import { db } from "./database.js";
 
-console.log("firestore connected")
+const app = express();
+const PORT = 3002;
 
-const app = express()
-const PORT = 3002
 
-app.get("/get", (req, res) => {
-  res.send("Hej grupp 2");
+app.use(express.json());
+
+
+  app.get("/getTodos", async (req, res) => {
+  try {
+    const querySnapshot = await getDocs(collection(db, "todos"));
+
+    const todos = querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+
+    res.status(200).json(todos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Kunde inte hämta todos" });
+  }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
