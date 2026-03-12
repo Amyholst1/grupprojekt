550afc211f55322c83497436fe5752fdb0d50577
JK Nordstrand <joohannees@hotmail.se>
2026-03-12 13:33:23 +0100

    Add filter component for all, active and completed tasks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import { useState } from 'react'
 import Navbar from './components/navbar'
 import AddTask from './components/AddTask'
+import Filter from './components/Filter/Filter'
 
 function App() {
+  const [selectedFilter, setSelectedFilter] = useState("All")
   return (
     <>
       <Navbar />
       <AddTask />
+      <Filter 
+      selectedFilter={selectedFilter}
+      setSelectedFilter={setSelectedFilter}
+      />
     </>
   )
 }
