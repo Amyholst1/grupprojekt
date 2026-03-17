136d91671f9b23370f5adbcd9c6a065583fb9072
Prasoon <prasoons97@gmail.com>
2026-03-17 12:39:34 +0100

    fix: added use of toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,35 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import { useState } from 'react'
-import Filter from './components/Filter/Filter'
-import TaskInput from './components/TaskInput/TaskInput'
-import Todolist from './components/Todolist/Todolist'
-
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
+import Filter from "./components/Filter/Filter";
+import TaskInput from "./components/TaskInput/TaskInput";
+import Todolist from "./components/Todolist/Todolist";
+import ToastMessage from "./components/ToastMessage/ToastMessage";
 
 function App() {
-  const [selectedFilter, setSelectedFilter] = useState("All")
-   const [tasks, setTasks] = useState([])
+  const [selectedFilter, setSelectedFilter] = useState("All");
+  const [toast, setToast] = useState(null);
 
-  function handleAddTask(newTask) {
-    setTasks((prevTasks) => [...prevTasks, newTask])
+  function showToast(message, type) {
+    setToast({ message, type });
+
+    setTimeout(() => {
+      setToast(null);
+    }, 3000);
   }
 
   return (
     <>
       <Navbar />
-      <TaskInput onAddTask={handleAddTask}/>
-      <Filter 
-      selectedFilter={selectedFilter}
-      setSelectedFilter={setSelectedFilter}
+      <TaskInput showToast={showToast} />
+      <Filter
+        selectedFilter={selectedFilter}
+        setSelectedFilter={setSelectedFilter}
       />
       <Todolist></Todolist>
+      {toast && <ToastMessage message={toast.message} type={toast.type} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
